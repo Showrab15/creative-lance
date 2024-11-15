d0b921c8e49416fbb1126efce099322ae7a87b25
Showrab15 <supto50showrab50showrab@gmail.com>
2024-11-16 02:14:27 +0600

    project Setup Done

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+           <h1 className="text-5xl text-center text-green-500 my-10">Welcome to Creative Lance</h1> 
+        </div>
+    );
+};
+
+export default MainLayout;
